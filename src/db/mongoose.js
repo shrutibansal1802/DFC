4195b81ab5434070e341638faab285f2f3cab529
shruti <shrutibansal1802@gmail.com>
2020-09-07 18:53:42 +0530

    needy event card

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose =require('mongoose');
+const uri = process.env.MONGO_URI;
 
-mongoose.connect('mongodb://127.0.0.1:27017/donorforhelp',{
+mongoose.connect(uri,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true
