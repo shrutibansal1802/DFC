55b8e0147af974507d8da48bc6813b6512241db7
shruti <shrutibansal1802@gmail.com>
2020-09-28 22:59:16 +0530

    moongoose change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 const uri = process.env.MONGO_URI;
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager',{
+mongoose.connect(uri,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true
